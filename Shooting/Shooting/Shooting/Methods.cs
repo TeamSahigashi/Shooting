82b350b07c3a8e3d5f9e9cad3ef477e78255c736
yorukaze <ono_u4ro@yahoo.co.jp>
2013-08-28 15:09:09 +0900

    メソッドだけかえた

--- Shooting/Shooting/Shooting/Methods.cs
+++ Shooting/Shooting/Shooting/Methods.cs
@@ -1,1 +1,67 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace Shooting
+{
+    public partial class Game1 : Microsoft.Xna.Framework.Game
+    {
+        /// <summary>
+        /// ゲームを開始する
+        /// </summary>
+        /// <param name="stagenum">ステージ番号</param>
+        void GameStart()
+        {
+            switch (stagenum)
+            {
+                case 0:
+                    Makestage1();
+                    break;
+                case 1:
+                    Makestage2();
+                    break;
+                case 2:
+                    Makestage3();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// １面設定
+        /// </summary>
+        void Makestage1()
+        {
+
+        }
+
+
+        /// <summary>
+        /// ２面設定
+        /// </summary>
+        void Makestage2()
+        {
+
+        }
+
+
+
+        /// <summary>
+        /// ３面設定
+        /// </summary>
+        void Makestage3()
+        {
+
+        }
+
+    }
+}
